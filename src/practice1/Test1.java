def91e926399a880cd8d379879f3b9709675c40b
Lenovo <Lenovo@Arsh>
2021-08-10 19:15:53 +0530

    Push2 by arshad

--- src/practice1/Test1.java
+++ src/practice1/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		System.out.println("HI Arshad and Monika");
+		System.out.println("Hi zhal ki bhawa");
+		System.out.println("Mission Success when we got 15 lac package");
 
 	}
 
