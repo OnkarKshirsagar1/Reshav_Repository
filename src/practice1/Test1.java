10951c2850f0ce3ab5c40f8bbfe057768b55bdea
Lenovo <Lenovo@Arsh>
2021-08-10 21:26:33 +0530

    hhh

--- src/practice1/Test1.java
+++ src/practice1/Test1.java
@@ -11,7 +11,7 @@ public class Test1 {
 		System.out.println("Congratulations Arshad Onkar, Monika for getting Package of 15LPA");
 
 		System.out.println("Push to Origin");
-		
+	
 		System.out.println("Continue kara sir pudhcha kaam mazh kaam zhalay aajacha");
 		System.out.println("hi sir");
 
