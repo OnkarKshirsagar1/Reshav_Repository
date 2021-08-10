408c295b1ea0b5de4910f73719520269b3d2a61c
Lenovo <Lenovo@Arsh>
2021-08-10 21:13:14 +0530

    arsh

--- src/practice1/Test1.java
+++ src/practice1/Test1.java
@@ -13,7 +13,7 @@ public class Test1 {
 		System.out.println("Push to Origin");
 		
 		System.out.println("Continue kara sir pudhcha kaam mazh kaam zhalay aajacha");
-		System.out.println("jsj");
+		System.out.println("hi");
 
 	}
 
